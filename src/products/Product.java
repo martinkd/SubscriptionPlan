968852707054fa2b9f23788f43f042c05ddb1cbc
Martin Dang <martin.k.dang@gmail.com>
2017-05-15 14:36:07 +0300

    Delete not used setters

--- src/products/Product.java
+++ src/products/Product.java
@@ -25,10 +25,6 @@ public class Product {
 		return name;
 	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
-
 	@Override
 	public String toString() {
 		return String.format("%s | %.2f", name, price);
--- src/rules/RangeBasedDiscount.java
+++ src/rules/RangeBasedDiscount.java
@@ -1,16 +1,16 @@
 package rules;
 
 public class RangeBasedDiscount {
-	
+
 	private String type;
 	private double range;
 	private double discount;
 
 	public RangeBasedDiscount() {
 	}
-	
+
 	public RangeBasedDiscount(String type, double range, double discount) {
-		
+
 		this.type = type;
 		this.range = range;
 		this.discount = discount;
@@ -20,25 +20,13 @@ public class RangeBasedDiscount {
 	public double getRange() {
 		return range;
 	}
-	
-	public void setRange(double range) {
-		this.range = range;
-	}
-	
+
 	public double getDiscount() {
 		return discount;
-	}
-	
-	public void setDiscount(double discount) {
-		this.discount = discount;
 	}
 
 	public String getType() {
 		return type;
-	}
-
-	public void setType(String type) {
-		this.type = type;
 	}
 
 }
