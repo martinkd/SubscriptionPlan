96a2eeb0fc88956f63bbfb5ea633883b604af411
Martin Dang <martin.k.dang@gmail.com>
2017-05-16 10:50:41 +0300

    Clean up the code

--- src/engine/Engine.java
+++ src/engine/Engine.java
@@ -3,7 +3,6 @@ package engine;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,14 +24,14 @@ public class Engine {
 		Engine e = new Engine();
 		Bundle bundle = e.getBundle("tv_net_xl");
 		
-		Engine.printBundle(bundle);
+		Engine.print(bundle);
 		
 		double before = bundle.getPrice();
 		System.out.printf("%s price before discount %.2f%n",bundle.getName(), before);
 		System.out.println("-------------");
 		
 		e.discount(bundle);
-		Engine.printBundle(bundle);
+		Engine.print(bundle);
 		System.out.printf("%s price after product discounts %.2f%n",bundle.getName(), bundle.getPrice());
 		System.out.println("-------------");
 		
@@ -80,7 +79,7 @@ public class Engine {
 
 	}
 
-	private static void printBundle(Bundle bundle) {
+	private static void print(Bundle bundle) {
 		System.out.println(bundle.getName());
 		for (Product p : bundle.getProducts()) {
 			System.out.println(p);
@@ -94,11 +93,32 @@ public class Engine {
 		setProductsForBundle(bundle);
 		return bundle;
 	}
+	
+	private void setProductsForBundle(Bundle bundle) throws SQLException {
+		
+		String getProductsFromBundle = "SELECT products.name, products.price " 
+				+ "FROM products "
+				+ "JOIN bundle_products " 
+				+ "ON products.id = bundle_products.product_id " 
+				+ "JOIN bundles "
+				+ "ON bundles.id = bundle_products.bundle_id " 
+				+ "WHERE bundles.name = ?";
+		PreparedStatement prepSt = DatabaseConnection.getConnection().prepareStatement(getProductsFromBundle);
+		prepSt.setString(1, bundle.getName());
+		ResultSet rs = prepSt.executeQuery();
+		
+		while (rs.next()) {
+			String name = rs.getString("name");
+			double price = rs.getDouble("price");
+			Product product = new Product(name, price);
+			bundle.add(product);
+		}
+	}
 
 	public List<Bundle> getAllBundles() throws SQLException {
 
 		String getBundles = "SELECT name FROM bundles";
-		Statement stat = DatabaseConnection.getConnection().createStatement();
+		PreparedStatement stat = DatabaseConnection.getConnection().prepareStatement(getBundles);
 		ResultSet rs = stat.executeQuery(getBundles);
 
 		List<Bundle> bundles = new ArrayList<Bundle>();
@@ -121,26 +141,12 @@ public class Engine {
 		}
 	}
 
-	private void setProductsForBundle(Bundle bundle) throws SQLException {
-
-		String getProductsFromBundle = "SELECT products.name, products.price " + "FROM products "
-				+ "JOIN bundle_products " + "ON products.id = bundle_products.product_id " + "JOIN bundles "
-				+ "ON bundles.id = bundle_products.bundle_id " + "WHERE bundles.name = ?";
-		PreparedStatement prepSt = DatabaseConnection.getConnection().prepareStatement(getProductsFromBundle);
-		prepSt.setString(1, bundle.getName());
-		ResultSet rs = prepSt.executeQuery();
-
-		while (rs.next()) {
-			String name = rs.getString("name");
-			double price = rs.getDouble("price");
-			Product product = new Product(name, price);
-			bundle.add(product);
-		}
-	}
 
 	private List<RangeBasedDiscount> getDiscounts(String type) throws SQLException {
 
-		String query = "SELECT disc_range, discount " + "FROM discounts " + "WHERE type = ?"
+		String query = "SELECT disc_range, discount " 
+				+ "FROM discounts " 
+				+ "WHERE type = ?"
 				+ "ORDER BY disc_range DESC";
 		PreparedStatement prepSt = DatabaseConnection.getConnection().prepareStatement(query);
 		prepSt.setString(1, type);
